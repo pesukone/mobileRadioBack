390706065ed6b97853e3b68e48f81e2a2cc2c80d
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2018-04-01 16:43:17 +0300

    tried to make node read from fifo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
@@ -7,6 +8,14 @@ const server = http.createServer(app)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/audio', (req, res) => {
+  const fd = fs.openSync('Scripts/input_pipe', 'r')
+  const stream = fs.createReadStream(null, { fd })
+  stream.on('data', (d) => {
+    console.log(d.toString())
+  })
 })
 
 server.listen(3000, () => {
